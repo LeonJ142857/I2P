9c2f5a474df2d2334af191be92755e524b535373
LeonJ142857 <71361983+LeonJ142857@users.noreply.github.com>
2020-12-19 14:59:11 +0700

    changed name to classic_interface and some refactoring

---
+++ Matrix_Calculator/classic_interface.py
@@ -0,0 +1,8 @@
+from interface_appear_classic import *
+from grid_configure_weight import *
+root.minsize(350, 450)
+grid_configures(root, 8, 5)
+appear_classic()
+###########################################
+
+root.mainloop()
