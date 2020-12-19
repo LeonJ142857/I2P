c94907869181169ccefd87a2532e622bf2f18800
LeonJ142857 <71361983+LeonJ142857@users.noreply.github.com>
2020-12-19 15:52:45 +0700

    moved the file named 'classic_buttons_positioning' to configurations

--- Matrix_Calculator/classic_interface.py
+++ Matrix_Calculator/classic_interface.py
@@ -1,4 +1,3 @@
-from root_window import root_window
 from classic_buttons_positioning import *
 from grid_configure_weight import *
 from button_objects_classic import *
@@ -8,5 +7,4 @@ grid_configures(root_window, 8, 5)
 classic_buttons_positioning(b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, pi, e, unary, binary, CE, backspace,
 								add, subtract, multiply, divide, modulo, equal, abs, a_pow_2,  a_pow_b,
 								comma, exp, fact, log, ln, one_over_a, plus_min, sq_root, ten_pow_a)
-
 root_window.mainloop()
